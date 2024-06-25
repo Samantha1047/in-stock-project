075da95e5f2f426579181bbbc619248e73b763dd
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-25 09:36:05 -0700

    create: form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,16 @@
+import FormButtons from "../FormButtons/FormButtons";
+import "./Form.scss";
+import WarehouseForm from "../WarehouseForm/WarehouseForm";
+
+const Form = ({ page, onSubmit }) => {
+  return (
+    <form className="warehouse__form">
+      {page === "warehouse" && <WarehouseForm />}
+      {page === "inventory" && <h1>Inventory Form</h1>}
+
+      <FormButtons />
+    </form>
+  );
+};
+
+export default Form;
