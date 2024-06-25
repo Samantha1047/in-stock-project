8398f0bc4f8f81b214e566e9b2ce90dd93b47a35
Jasmina Savkovic <jsavkovi@gmail.com>
2024-06-25 01:09:46 -0400

    Create header component.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../../assets/logo/InStock-Logo.svg'
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <header>
+
+        </header>
+    )
+}
+
+export default Header;
