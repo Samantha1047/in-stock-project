1896d627d50c473b1b9a3a64ae72fb6078f3708b
Samantha Sun <samisun1047@gmail.com>
2024-06-25 10:24:10 -0700

    change responsive styles

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -2,7 +2,11 @@ import "./WarehouseDetails.scss";
 import WarehouseDetail from "../../components/WarehouseDetail/WarehouseDetail";
 
 const WarehouseDetails = () => {
-  return <WarehouseDetail />;
+  return (
+    <main>
+      <WarehouseDetail />
+    </main>
+  );
 };
 
 export default WarehouseDetails;
