024614d758bce2f73f4025b7ff6cfd1675bb631c
Samantha Sun <samisun1047@gmail.com>
2024-06-24 18:51:45 -0700

    add footer component and style

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <section className="footer">
+        <p>© InStock Inc. All Rights Reserved.</p>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
