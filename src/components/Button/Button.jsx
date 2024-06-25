eff8e3decc01acd2f5dffd1ed0c9ce53ff11bb73
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-25 13:21:41 -0700

    fix: button according to location

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,26 @@
+import "./Button.scss";
+
+const Button = ({ text, icon, isPrimary, type, location }) => {
+  const isPrimaryClass = isPrimary ? "button--primary" : "button--secondary";
+  const isPrimaryText = isPrimary
+    ? "button--primary-text"
+    : "button--secondary-text";
+  const buttonLocation =
+    location === "form" ? "button--form" : "button--default";
+  console.log(location);
+
+  //Might need to be adjusted if we include edit button
+  return (
+    <button
+      type={type}
+      className={`button ${isPrimaryClass} ${buttonLocation}`}
+    >
+      <div className="button__icon-container">
+        <img className="button__icon" alt={icon} src={icon} />
+      </div>
+      <p className={`button__text ${isPrimaryText}`}>{text}</p>
+    </button>
+  );
+};
+
+export default Button;
