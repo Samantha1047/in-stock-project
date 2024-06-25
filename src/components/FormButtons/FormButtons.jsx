57e6dfe6b65787f254dd8aee99915b881bd3eb1b
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-25 09:36:33 -0700

    create: form buttons component

---
+++ src/components/FormButtons/FormButtons.jsx
@@ -0,0 +1,10 @@
+const FormButtons = () => {
+  return (
+    <section className="form-buttons">
+      <button type="reset">Cancel</button>
+      <button type="submit">Save</button>
+    </section>
+  );
+};
+
+export default FormButtons;
