e9b819a9786c12afd76613994c5599e65af87d90
Jasmina Savkovic <jsavkovi@gmail.com>
2024-06-25 13:15:37 -0400

    Fetch warehouse data from server api.

--- src/components/Warehouse/WarehouseList.jsx
+++ src/components/Warehouse/WarehouseList.jsx
@@ -1,8 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react-router-dom'
+import TableHeader from '../TableHeader/TableHeader'
+import editIcon from '../../assets/icons/edit-24px.svg'
+import chevRight from '../../assets/icons/chevron_right-24px.svg'
+import './WarehouseList.scss'
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
 
-function WarehouseList({ }) {
+const WarehouseList = ({ setCurrentWarehouse }) => {
+    const [warehouseList, setWarehouseList] = useState([]);
+    const navigate = useNavigate();
 
-    const tableHeaders = ["warehouse"]
+    useEffect(() => {
+        const fetchWarehouses = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/api/warehouses`);
+                setWarehouseList(response.data);
+            } catch (error) {
+                console.error('Error fetching warehouse data: ', error)
+            }
+        };
+        fetchWarehouses();
+    }, []);
+
+    const tableHeaders = ['WAREHOUSE', 'ADDRESS', 'CONTACT NAME', 'CONTACT INFORMATION', 'ACTIONS']
+    const item = { id, warehouse_name, city, country, contact_name, contact_phone, contact_email }
 
     return (
         <section></section>
