0e69821cc245fdc5b75a4fb73f1ce59c8d0fc160
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-26 16:58:29 -0700

    create: inventory edit page

--- src/pages/InventoryEdit/InventoryEdit.jsx
+++ src/pages/InventoryEdit/InventoryEdit.jsx
@@ -1,7 +1,14 @@
 import "./InventoryEdit.scss";
+import Form from "../../components/Form/Form";
+import TitleNav from "../../components/TitleNav/TitleNav";
 
 const InventoryEdit = () => {
-  return <div>InventoryEdit</div>;
+  return (
+    <div className="inventory-edit">
+      <TitleNav title="Edit Inventory Item" page="Inventory" edit={false} />
+      <Form mode="edit" page="inventory" />
+    </div>
+  );
 };
 
 export default InventoryEdit;
