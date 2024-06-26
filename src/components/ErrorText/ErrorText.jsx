1d5a7648a94e75800a3a837e998c8f017fd6f171
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-25 22:29:58 -0700

    add: dynamic text to error text

---
+++ src/components/ErrorText/ErrorText.jsx
@@ -0,0 +1,14 @@
+import "./ErrorText.scss";
+import errorIcon from "../../assets/icons/error-24px.svg";
+
+const ErrorText = ({ text }) => {
+  const errorText = text ? text : "This field is required";
+  return (
+    <div className={`error-text `}>
+      <img className="error-text__icon" src={errorIcon} alt="error icon" />
+      <p className="error-text__body">{errorText}</p>
+    </div>
+  );
+};
+
+export default ErrorText;
