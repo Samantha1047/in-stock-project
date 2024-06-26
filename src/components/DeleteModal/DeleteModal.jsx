71a57bf3a2a24d1544bc2fc797ffb7040b40a2c0
Samantha Sun <samisun1047@gmail.com>
2024-06-26 11:17:42 -0700

    adjust code for tablet and desktop view

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -4,15 +4,17 @@ import Button from "../Button/Button";
 const DeleteModal = ({ WarehouseName /* onclose, onConfirm */ }) => {
   return (
     <section className="modal">
-      <i className="modal__closeIcon" /*  onClick={onclose} */ />
-      <div className="modal__content">
-        <h1>Delete {WarehouseName} warehouse?</h1>
-        <p>Please confirm that you’d like to delete the {WarehouseName} from the list of warehouses. You won’t be able to undo this action.</p>
-      </div>
+      <div className="modal__container">
+        <i className="modal__closeIcon" /*  onClick={onclose} */ />
+        <div className="modal__content">
+          <h1>Delete {WarehouseName} warehouse?</h1>
+          <p>Please confirm that you’d like to delete the {WarehouseName} from the list of warehouses. You won’t be able to undo this action.</p>
+        </div>
 
-      <div className="modal__buttons">
-        <Button text="Cancel" isPrimary={false} /*  onClick={onclose} */ />
-        <button /* onClick={onConfirm} */ className="modal__delete-button">Delete</button>
+        <div className="modal__buttons">
+          <Button text="Cancel" isPrimary={false} /*  onClick={onclose} */ />
+          <button /* onClick={onConfirm} */ className="modal__delete-button">Delete</button>
+        </div>
       </div>
     </section>
   );
