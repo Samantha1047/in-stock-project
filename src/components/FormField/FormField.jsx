34f6021cbf9c0cd40b72941eea67284ef7189848
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-26 22:25:34 -0700

    update: formfield component to take account textareas

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -8,22 +8,31 @@ const FormField = ({
   onChange,
   placeholder,
   error,
-}) => (
-  <fieldset className="form-field__input-container">
-    <label className="form-field__input-label" htmlFor={name}>
-      {label}
-    </label>
-    <input
-      type={type}
-      className={`form-field__input ${error ? "error-border" : ""}`}
-      id={name}
-      placeholder={placeholder}
-      name={name}
-      onChange={onChange}
-      value={value}
-    />
-    {error && <ErrorText />}
-  </fieldset>
-);
+  input,
+  size,
+}) => {
+  const inputType = input === "textarea" ? "form-field--textarea" : "";
+  const inputSize = size === "small" ? "form-field__input--small" : "";
+
+  return (
+    <fieldset className={`form-field__input-container `}>
+      <label className="form-field__input-label" htmlFor={name}>
+        {label}
+      </label>
+      <textarea
+        type={type}
+        className={`form-field__input ${
+          error ? "error-border" : ""
+        } ${inputSize} ${inputType} `}
+        id={name}
+        placeholder={placeholder}
+        name={name}
+        onChange={onChange}
+        value={value}
+      />
+      {error && <ErrorText />}
+    </fieldset>
+  );
+};
 
 export default FormField;
