50ac3aa4d3e38d5fc8c2aa60beefec427985e700
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-26 22:26:16 -0700

    add: constants

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const FORM_TYPES = {
+  WAREHOUSE: "warehouse",
+  INVENTORY: "inventory",
+};
+
+export const FORM_MODES = {
+  ADD: "add",
+  EDIT: "edit",
+};
+
+export const INVENTORY_STOCK = {
+  IN_STOCK: "In Stock",
+  OUT_OF_STOCK: "Out of Stock",
+};
