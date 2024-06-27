0fbf8c720ac4fba28392edd84205d0a3063d4afc
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-27 12:02:53 -0700

    update: dropdown component

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -1,22 +1,9 @@
 import ErrorText from "../ErrorText/ErrorText";
 import chevronDown from "../../assets/icons/arrow_drop_down-24px.svg";
-const categoryOptions = [
-  "Electronics",
-  "Accessories",
-  "Gear",
-  "Apparel",
-  "Health",
-];
 
-//Need to grab warehouse names from the database, use this as a placeholder
-const warehouseOptions = [
-  { warehouse_name: "Manhattan", warehouse_id: 1 },
-  { warehouse_name: "Washington", warehouse_id: 2 },
-  { warehouse_name: "Jersey", warehouse_id: 3 },
-];
 import "./DropDown.scss";
 import { useState } from "react";
-const DropDown = ({ name, handleInputChange, value, errors }) => {
+const DropDown = ({ name, handleInputChange, value, errors, categoryList }) => {
   const [isFocused, setIsFocused] = useState(false);
 
   const handleFocus = () => {
@@ -26,8 +13,6 @@ const DropDown = ({ name, handleInputChange, value, errors }) => {
   const handleBlur = () => {
     setIsFocused(false);
   };
-  const dropdownOptions =
-    name === "category" ? categoryOptions : warehouseOptions;
   return (
     <div tabIndex="0" className={`drop-down ${isFocused ? "focused" : ""}`}>
       <select
@@ -36,17 +21,11 @@ const DropDown = ({ name, handleInputChange, value, errors }) => {
         onChange={handleInputChange}
         name={name}
       >
-        {dropdownOptions.map((option) => {
-          const optionKey = name === "category" ? option : option.warehouse_id;
-          const optionValue =
-            name === "category" ? option : option.warehouse_name;
-
-          return (
-            <option key={optionKey} value={optionKey}>
-              {optionValue}
-            </option>
-          );
-        })}
+        {categoryList.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
       </select>
       <img
         className="drop-down__icon"
