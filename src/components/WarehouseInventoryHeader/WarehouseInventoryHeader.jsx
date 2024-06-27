12638dcb8cfedbce07b190b089d606de4d4e509a
Jasmina Savkovic <jsavkovi@gmail.com>
2024-06-26 22:10:41 -0400

    Set up folders for new components.

---
+++ src/components/WarehouseInventoryHeader/WarehouseInventoryHeader.jsx
@@ -0,0 +1,11 @@
+import './WarehouseInventoryHeader.scss'
+
+const ComponentName = () => {
+    return (
+        <div>
+      /* JSX goes here */
+        </div>
+    );
+};
+
+export default ComponentName;
