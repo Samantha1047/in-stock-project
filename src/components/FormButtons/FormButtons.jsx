8d3a3f59ff9443a10b09bf484ebd76f9cc7a7ce8
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-27 17:19:27 -0700

    update: form buttons for add item

--- src/components/FormButtons/FormButtons.jsx
+++ src/components/FormButtons/FormButtons.jsx
@@ -1,8 +1,17 @@
 import Button from "../Button/Button";
 import "./FormButtons.scss";
 
-const FormButtons = ({ handleSubmit, mode }) => {
-  const buttonText = mode === "add" ? "+ Add Warehouse" : "Save";
+const FormButtons = ({ handleSubmit, mode, page }) => {
+  const buttonTextMap = {
+    "add-inventory": "+ Add Item",
+    "add-warehouse": "+ Add Warehouse",
+    edit: "Save",
+  };
+
+  const key = mode === "add" ? `add-${page}` : mode;
+
+  const buttonText = buttonTextMap[key] || "Save";
+
   return (
     <section className="form-buttons">
       <Button location="form" text="Cancel" isPrimary={false} type="reset" />
