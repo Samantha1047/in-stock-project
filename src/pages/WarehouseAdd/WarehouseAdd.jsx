c91a2d83558ced963d0c3131d64b688beafafda9
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-28 19:26:29 -0700

    fix: prop warehouse to warehouse edit and add page

--- src/pages/WarehouseAdd/WarehouseAdd.jsx
+++ src/pages/WarehouseAdd/WarehouseAdd.jsx
@@ -2,11 +2,14 @@ import "./WarehouseAdd.scss";
 import TitleNav from "../../components/TitleNav/TitleNav";
 import WarehouseForm from "../../components/WarehouseForm/WarehouseForm";
 
-const WarehouseAdd = ({ warehouses }) => {
+const WarehouseAdd = ({ warehouses, handleWarehouseSubmit }) => {
   return (
     <article className="warehouse-add">
       <TitleNav title="Add New Warehouse" page="Warehouse" edit={false} />
-      <WarehouseForm mode="add" warehouses={warehouses} />
+      <WarehouseForm
+        mode="add"
+        warehouses={warehouses}
+      />
     </article>
   );
 };
