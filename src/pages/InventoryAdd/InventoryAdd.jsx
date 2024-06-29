cbf457c28cb546b8084f956262c171a326f24f25
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-28 18:32:57 -0700

    fix: add warehouses to inventory add page

--- src/pages/InventoryAdd/InventoryAdd.jsx
+++ src/pages/InventoryAdd/InventoryAdd.jsx
@@ -2,11 +2,15 @@ import "./InventoryAdd.scss";
 import TitleNav from "../../components/TitleNav/TitleNav";
 import InventoryForm from "../../components/InventoryForm/InventoryForm";
 
-const InventoryAdd = ({ handleInventorySubmit }) => {
+const InventoryAdd = ({ handleInventorySubmit, warehouses }) => {
   return (
     <div className="inventory-add">
       <TitleNav title="Add Inventory Item" page="Inventory" edit={false} />
-      <InventoryForm mode="add" handleInventorySubmit={handleInventorySubmit} />
+      <InventoryForm
+        mode="add"
+        warehouses={warehouses}
+        handleInventorySubmit={handleInventorySubmit}
+      />
     </div>
   );
 };
