448fcb0bee13a09c82f51ba8bb63785afb3fea0c
Jasmina Savkovic <jsavkovi@gmail.com>
2024-06-29 16:01:10 -0400

    update SearchBar component. Create PageHeader component.

--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -1,37 +1,12 @@
 import "./Warehouse.scss";
-
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
-import searchIcon from "../../assets/icons/search-24px.svg";
-import { useNavigate } from "react-router-dom";
-import SearchBar from "../../components/SearchBar/SearchBar";
+import PageHeader from '../../components/PageHeader/PageHeader'
 
 const Warehouse = ({ warehouses, handleWarehouseDelete }) => {
-  const navigate = useNavigate();
 
   return (
     <main className="warehouse">
-      <div className="warehouse__header">
-        <h1 className="warehouse__title">Warehouses</h1>
-        <div>
-          {/* <input
-            type="text"
-            placeholder="Search..."
-            className="warehouse__search"
-          />
-          <img
-            className="warehouse__search-icon"
-            src={searchIcon}
-            alt="search icon"
-          /> */}
-          <SearchBar />
-          <button
-            onClick={() => navigate("/add")}
-            className="warehouse__button"
-          >
-            + Add New Warehouse
-          </button>
-        </div>
-      </div>
+      <PageHeader title='Warehouse' link='/add' buttonTitle='+ Add New Warehouse' />
       <WarehouseList
         warehouses={warehouses}
         handleWarehouseDelete={handleWarehouseDelete}
