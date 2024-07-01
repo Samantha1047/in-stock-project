e5d042a6560fa45e312c80d4398a7494327003a6
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-01 04:56:19 -0700

    chore: clean code not being used

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,11 @@ import "./Button.scss";
 
 const Button = ({ text, icon, isPrimary, type, location, handleEvent }) => {
   const isPrimaryClass = isPrimary ? "button--primary" : "button--secondary";
+  const buttonLocation = location ? `button__icon--${location}` : "";
+  const buttonClass = icon
+    ? `button__icon ${buttonLocation}`
+    : `button ${isPrimaryClass} ${buttonLocation}`;
 
-  const buttonLocation = location ? `button__icon--${location}` : "button--default";
-
-  const buttonClass = icon ? `button__icon ${buttonLocation}` : `button ${isPrimaryClass} ${buttonLocation}`;
   return (
     <button type={type} className={buttonClass} onClick={handleEvent}>
       {icon && (
@@ -13,7 +14,7 @@ const Button = ({ text, icon, isPrimary, type, location, handleEvent }) => {
           <img className="button__icon-img" alt={icon} src={icon} />
         </div>
       )}
-      {!icon && <p className={`button__text`}>{text}</p>}
+      {!icon && <p className="button__text">{text}</p>}
     </button>
   );
 };
